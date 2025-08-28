b949e5409f31fff75bbd2655086b8922b5511f9c
Matias Galvan Guerrerogit config --global user.email matias.galvan@tecsup.edu.p <matias.galvan@tecsup.edu.pe>
2025-08-28 15:22:30 -0500

    Update StudentModel.java in sprint-1 branch

--- src/StudentModel.java
+++ src/StudentModel.java
@@ -1,5 +1,19 @@
 package src;
 
 public class StudentModel {
+    private String name;
+    private int id;
     
+    public StudentModel(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+    
+    // Método agregado en sprint-1
+    public void displayInfo() {
+        System.out.println("Student: " + name + ", ID: " + id);
+    }
+    
+    public String getName() { return name; }
+    public int getId() { return id; }
 }
